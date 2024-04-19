5ef45af1a557260dfde31b594af308185e71d4eb
rpi2 <078bei018.kundan@pcampus.edu.np>
2024-04-20 01:14:45 +0545

    Added wait actio client node

---
+++ include/waitActionClient.hpp
@@ -0,0 +1,51 @@
+#ifndef WAIT_ACTION_CLIENT_HPP
+#define WAIT_ACTION_CLIENT_HPP
+
+#include <string>
+#include <memory>
+
+#include "nav2_msgs/action/wait.hpp"
+#include "std_msgs/msg/int32.hpp"
+// #include "std_msgs/msg/uint32.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+
+class waitActionClient : public BT::StatefulActionNode
+{
+    public:
+    waitActionClient(
+        const std::string &name,
+        const BT::NodeConfiguration &config,
+        rclcpp::Node::SharedPtr node_ptr);
+
+    using Wait = nav2_msgs::action::Wait;
+    using GoalHandleWait = rclcpp_action::ClientGoalHandle<Wait>;
+    using int32 = std_msgs::msg::Int32;
+
+
+    rclcpp::Node::SharedPtr node_ptr_;
+    rclcpp_action::Client<Wait>::SharedPtr action_client_ptr_;
+
+    Wait::Goal wait;
+    bool done_flag;
+
+    // Methods override (uncomment if you have ports to I/O data)
+    static BT::PortsList providedPorts();
+
+    BT::NodeStatus onStart() override;
+    BT::NodeStatus onRunning() override;
+    void onHalted() override;
+
+
+    // Action client result callback
+    void wait_result_callback(const GoalHandleWait::WrappedResult &result);
+
+    // Action client feedback callback
+    void wait_feedback_callback(GoalHandleWait::SharedPtr, const std::shared_ptr<const Wait::Feedback> feedback);
+    
+};
+
+#endif
---
+++ src/waitActionClient.cpp
@@ -0,0 +1,97 @@
+#include "waitActionClient.hpp"   
+
+waitActionClient::waitActionClient(
+    const std::string &name,
+    const BT::NodeConfiguration &config,
+    rclcpp::Node::SharedPtr node_ptr) 
+    : BT::StatefulActionNode(name,config), node_ptr_(node_ptr)
+{
+    action_client_ptr_ = rclcpp_action::create_client<Wait>(node_ptr_, "/wait");
+    RCLCPP_INFO(node_ptr_->get_logger(),"waitActionClient node created..");
+    done_flag = false;
+}
+
+BT::PortsList waitActionClient::providedPorts()
+{
+    return {
+        BT::InputPort<int32>("In_sec", "wait secs"),
+        BT::InputPort<uint32_t>("In_nanosec","wait nanosecs"),
+    };
+}
+
+BT::NodeStatus waitActionClient::onStart()
+{
+    auto seconds = getInput<int32>("In_sec");
+    auto nanoseconds = getInput<uint32_t>("In_nanosec");
+    if( !seconds )
+    {
+        throw BT::RuntimeError("error reading port [In_sec]:", seconds.error());
+    }
+    if( !nanoseconds)
+    {
+        throw BT::RuntimeError("error reading port [In_nanosec]:", nanoseconds.error());
+    }
+    
+    // Setup action client goal
+    auto send_goal_options = rclcpp_action::Client<Wait>::SendGoalOptions();
+    send_goal_options.result_callback = std::bind(&waitActionClient::wait_result_callback, this, std::placeholders::_1);
+    send_goal_options.feedback_callback = std::bind(&waitActionClient::wait_feedback_callback, this, std::placeholders::_1,std::placeholders::_2);
+
+    // make goal::target_yaw
+    wait.time.sec = seconds.value().data;
+    wait.time.nanosec = nanoseconds.value();
+
+    // send goal::target_yaw
+    done_flag = false;
+    action_client_ptr_->async_send_goal(wait, send_goal_options);
+    RCLCPP_INFO(node_ptr_->get_logger(),"sent goal to waitActionServer [%i seconds]", wait.time.sec);
+    return BT::NodeStatus::RUNNING;
+}
+
+BT::NodeStatus waitActionClient::onRunning()
+{   
+    if(done_flag)
+    {
+        RCLCPP_INFO(node_ptr_->get_logger(),"[%s] goal reached [waited: %i seconds] \n", this->name().c_str(), wait.time.sec);
+        return BT::NodeStatus::SUCCESS;
+    }
+    return BT::NodeStatus::RUNNING;
+}
+
+void waitActionClient::onHalted() 
+{
+     RCLCPP_WARN(node_ptr_->get_logger(),"Rotation aborted");
+}
+
+void waitActionClient::wait_result_callback(const GoalHandleWait::WrappedResult &result)
+{
+    if(result.result)
+        done_flag=true;
+}
+
+void waitActionClient::wait_feedback_callback(
+    GoalHandleWait::SharedPtr,
+    const std::shared_ptr<const Wait::Feedback> feedback)
+{
+    RCLCPP_INFO(node_ptr_->get_logger(),"[angular_distance_travelled: %i]",feedback->time_left.sec);
+}
+
+//  Implemented from documentation: Generic port
+namespace BT
+{
+    template <> inline std_msgs::msg::Int32 convertFromString(StringView str)
+    {
+        // We expect real numbers separated by semicolons
+        auto parts = splitString(str, '\n');
+        if (parts.size() != 1)
+        {
+            throw RuntimeError("invalid input)");
+        }
+        else
+        {
+            std_msgs::msg::Int32 output;
+            output.data    = convertFromString<float>(parts[0]);
+            return output;
+        }
+    }
+} // end names
