93d3b2789d1c405d5052f6fb8e5de1e625459fbc
rpi2 <078bei018.kundan@pcampus.edu.np>
2024-05-06 02:03:32 +0545

    Minor changes

--- include/InitiallizeActuators.hpp
+++ include/InitiallizeActuators.hpp
@@ -4,9 +4,9 @@
 #include <string>
 #include <memory>
 
-
 #include "rclcpp/rclcpp.hpp"
 #include "behaviortree_cpp_v3/bt_factory.h"
+#include "std_msgs/msg/u_int8.hpp"
 
 using namespace BT;
 
@@ -16,12 +16,17 @@ class InitiallizeActuators : public BT::SyncActionNode
     public:
     InitiallizeActuators(
         const std::string &name,
-        const BT::NodeConfiguration &config);
+        const BT::NodeConfiguration &config,
+        rclcpp::Node::SharedPtr node_ptr);
     
+    rclcpp::Node::SharedPtr node_ptr_;
+    std::shared_ptr<rclcpp::Subscription<std_msgs::msg::UInt8>> subscription_;
+
     // Methods override (uncomment if you have ports to I/O data)
     static BT::PortsList providedPorts();
-
     BT::NodeStatus tick() override;
+    
+    void subscriber_callback(std_msgs::msg::UInt8 msg);
     
 };
 
--- src/InitiallizeActuators.cpp
+++ src/InitiallizeActuators.cpp
@@ -2,9 +2,11 @@
 
 InitiallizeActuators::InitiallizeActuators(
     const std::string &name,
-    const BT::NodeConfiguration &config)
-    : BT::SyncActionNode(name,config)
+    const BT::NodeConfiguration &config,
+    rclcpp::Node::SharedPtr node_ptr)
+    : BT::SyncActionNode(name,config), node_ptr_(node_ptr)
 {
+    subscription_ = node_ptr_->create_subscription<std_msgs::msg::UInt8>( "/ball_roller_status", 10, std::bind(&InitiallizeActuators::subscriber_callback,this,std::placeholders::_1));
     RCLCPP_INFO(rclcpp::get_logger("InitiallizeActuators"),"InitiallizeActuators node Ready..");
 }
 
@@ -15,7 +17,9 @@ BT::PortsList InitiallizeActuators::providedPorts()
         BT::OutputPort<int>("Op_RollerSpeed"),   
         BT::OutputPort<bool>("Op_ConveyerStatus"),
         BT::OutputPort<int>("Op_ConveyerSpeed"), 
-        BT::OutputPort<bool>("Op_PneumaticStatus")
+        BT::OutputPort<bool>("Op_PneumaticStatus"),
+        BT::OutputPort<bool>("OP_IsBallInside"),
+        BT::OutputPort<bool>("OP_IsOnlyBall")
     };
 }
 
@@ -28,6 +32,30 @@ BT::PortsList InitiallizeActuators::providedPorts()
     setOutput<bool>("Op_PneumaticStatus", false);
     RCLCPP_INFO(rclcpp::get_logger("InitiallizeActuators"),"Actuators in reset state..");
     return BT::NodeStatus::SUCCESS;
+ }
+
+ void InitiallizeActuators::subscriber_callback(std_msgs::msg::UInt8 msg)
+ {
+    RCLCPP_INFO(rclcpp::get_logger("InitiallizeActuators"),"InitiallizeActuators subs callback..");
+    switch (msg.data)
+    {
+        case 1: 
+            RCLCPP_INFO(rclcpp::get_logger("InitiallizeActuators"),"Red Ball detected");
+            setOutput<bool>("OP_IsBallInside", true);
+            break;
+        case 2:
+            RCLCPP_INFO(rclcpp::get_logger("InitiallizeActuators"),"Blue Ball detected");
+            setOutput<bool>("OP_IsBallInside", false);
+            break;
+        case 3:
+            RCLCPP_INFO(rclcpp::get_logger("InitiallizeActuators"),"purple Ball detected");
+            setOutput<bool>("OP_IsBallInside", false);
+            break;
+
+        default:
+            RCLCPP_INFO(rclcpp::get_logger("InitiallizeActuators"),"No ball detected");
+            setOutput<bool>("OP_IsBallInside", false);
+    }
  }
 
 
