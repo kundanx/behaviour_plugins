cb99b2a1907cf2aa0095704272d28b5d328a9b50
rpi2 <078bei018.kundan@pcampus.edu.np>
2024-06-04 05:41:22 +0545

    Synced with stm32 pwm pins

--- src/actions/TurnOffConveyer.cpp
+++ src/actions/TurnOffConveyer.cpp
@@ -7,18 +7,23 @@ TurnOffConveyer::TurnOffConveyer(
 {
     // Mechanism is turnedOff intially
     setOutput<bool>("Op_ConveyerStatus", false);
+    setOutput<int>("Op_ConveyerSpeed", 1);
+
+
     RCLCPP_INFO(rclcpp::get_logger("TurnOffConveyer"),"TurnOffConveyer action Ready..");
 }
 
 BT::PortsList TurnOffConveyer::providedPorts()
 {
 return {
-    BT::OutputPort<bool>("Op_ConveyerStatus")
+    BT::OutputPort<bool>("Op_ConveyerStatus"),
+    BT::OutputPort<int>("Op_ConveyerSpeed")
 };   
 }
 BT::NodeStatus TurnOffConveyer::tick() 
 {
     setOutput<bool>("Op_ConveyerStatus", false);
+    setOutput<int>("Op_ConveyerSpeed", 1);
     RCLCPP_INFO(rclcpp::get_logger("TurnOffConveyer"),"Conveyer Turned Off..");
     return BT::NodeStatus::SUCCESS;
 }
--- src/actions/TurnOffRoller.cpp
+++ src/actions/TurnOffRoller.cpp
@@ -7,18 +7,23 @@ TurnOffRoller::TurnOffRoller(
 {
     // Mechanism is turnedOff intially
     setOutput<bool>("Op_RollerStatus", false);
+    setOutput<int>("Op_RollerSpeed", 1);
+    
     RCLCPP_INFO(rclcpp::get_logger("TurnOffRoller"),"TurnOffRoller action Ready..");
 }
 
 BT::PortsList TurnOffRoller::providedPorts()
 {
 return {
-    BT::OutputPort<bool>("Op_RollerStatus")
+    BT::OutputPort<bool>("Op_RollerStatus"),
+    BT::OutputPort<int>("Op_RollerSpeed")
 };   
 }
 BT::NodeStatus TurnOffRoller::tick() 
 {
     setOutput<bool>("Op_RollerStatus", false);
+    setOutput<int>("Op_RollerSpeed", 1);
+
     RCLCPP_INFO(rclcpp::get_logger("TurnOffRoller"),"Roller Turned Off..");
     return BT::NodeStatus::SUCCESS;
 }
