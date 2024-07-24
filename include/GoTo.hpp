454b7cf3460e8af7fde4e09e78ef3c0766a3aa77
apil <078bct017.apil@pcampus.edu.np>
2024-07-24 15:17:47 +0545

    Added go to any pose node

---
+++ include/GoTo.hpp
@@ -0,0 +1,69 @@
+#ifndef GO_TO_HPP
+#define GO_TO_HPP
+
+#include <string>
+#include <memory>
+#include <tf2/LinearMath/Quaternion.h>
+#include "behaviour_plugins/bezier.h"
+#include "behaviour_plugins/angle_conversions.hpp"
+#include "oakd_msgs/msg/state_pose.hpp"
+
+#include "std_msgs/msg/int8.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "nav2_msgs/action/navigate_through_poses.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+class GoTo : public BT::StatefulActionNode
+{
+    public:
+    GoTo(const std::string &name,
+             const BT::NodeConfiguration &config,
+             rclcpp::Node::SharedPtr node_ptr);
+
+    using NavigateToPose = nav2_msgs::action::NavigateToPose;
+    using GoalHandleNav = rclcpp_action::ClientGoalHandle<NavigateToPose>;
+    using PosMsg = geometry_msgs::msg::PoseStamped;
+
+
+    rclcpp::Node::SharedPtr node_ptr_;
+    rclcpp_action::Client<NavigateToPose>::SharedPtr action_client_ptr_;
+    rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr color_feedback_publisher;
+
+    std::shared_ptr<rclcpp::Subscription<std_msgs::msg::Int8>> subscription_team_color;  
+    std::shared_ptr<GoalHandleNav> goal_handle = nullptr;
+
+    enum TeamColor
+    {
+        RED = -1,
+        BLUE = 1
+    }team_color;
+   
+    bool done_flag;
+    NavigateToPose::Goal goal_msg;
+
+    // Methods override (uncomment if you have ports to I/O data)
+    static BT::PortsList providedPorts();
+
+    BT::NodeStatus onStart() override;
+    BT::NodeStatus onRunning() override;
+    void onHalted() override;
+
+    void cancel_goal();                    
+    void nav_to_pose_compute_goal();
+
+    // Subscribers callback
+    void team_color_callback(const std_msgs::msg::Int8 &msg);
+
+    // Action client feedbacks
+    void goal_response_callback(const GoalHandleNav::SharedPtr &goal_handle_);
+    void goal_result_callback(const GoalHandleNav::WrappedResult &result);
+    void goal_feedback_callback(GoalHandleNav::SharedPtr, const std::shared_ptr<const NavigateToPose::Feedback> feedback);
+    
+};
+
+#endif
---
+++ src/GoTo.cpp
@@ -0,0 +1,156 @@
+#include "GoTo.hpp"   
+
+/*****************************************************************************************************************
+ * @brief Navigate to ball position after ball position is recieved 
+******************************************************************************************************************/
+GoTo::GoTo(
+    const std::string &name,
+    const BT::NodeConfiguration &config,
+    rclcpp::Node::SharedPtr node_ptr) 
+    : BT::StatefulActionNode(name,config), node_ptr_(node_ptr)
+{
+    rclcpp::QoS qos_profile(10);
+    qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+    action_client_ptr_ = rclcpp_action::create_client<NavigateToPose>(node_ptr_, "/navigate_to_pose");
+
+    color_feedback_publisher = node_ptr_->create_publisher<std_msgs::msg::Int8>("color_feedback/GoTo", qos_profile);
+ 
+    subscription_team_color = node_ptr_->create_subscription<std_msgs::msg::Int8>(
+        "team_color",
+        qos_profile,
+        std::bind(&GoTo::team_color_callback, this, std::placeholders::_1)
+    );
+
+    done_flag = false;
+    RCLCPP_INFO(node_ptr_->get_logger(),"GoTo node Ready..");
+}
+
+BT::PortsList GoTo::providedPorts()
+{
+    return {BT::InputPort<float>("x"),
+            BT::InputPort<float>("y"),
+            BT::InputPort<float>("theta")};
+}
+
+BT::NodeStatus GoTo::onStart()
+{  
+
+    nav_to_pose_compute_goal();
+
+    auto send_goal_options = rclcpp_action::Client<NavigateToPose>::SendGoalOptions();
+    send_goal_options.goal_response_callback = std::bind(&GoTo::goal_response_callback, this, std::placeholders::_1);
+    send_goal_options.result_callback = std::bind(&GoTo::goal_result_callback, this, std::placeholders::_1);
+    send_goal_options.feedback_callback = std::bind(&GoTo::goal_feedback_callback, this, std::placeholders::_1,std::placeholders::_2);
+
+    action_client_ptr_->async_send_goal(goal_msg, send_goal_options);
+
+    done_flag = false;
+    RCLCPP_INFO(node_ptr_->get_logger(),"GoTo::sent goal");
+    return BT::NodeStatus::RUNNING;
+}
+
+BT::NodeStatus GoTo::onRunning()
+{     
+    if(done_flag)
+    {
+        RCLCPP_INFO(node_ptr_->get_logger(),"[%s] Goal reached", this->name().c_str());
+        return BT::NodeStatus::SUCCESS;
+    }
+    return BT::NodeStatus::RUNNING;
+}
+
+void GoTo::onHalted() 
+{
+    cancel_goal();
+}
+
+void GoTo::goal_response_callback(const GoalHandleNav::SharedPtr &goal_handle_)
+{
+    if (!goal_handle_)
+    {
+        RCLCPP_ERROR(node_ptr_->get_logger(), "GoTo::Navigate to ball pose rejected");
+    }
+    else
+    {
+        RCLCPP_INFO(node_ptr_->get_logger(), "GoTo::Navigating to given pose ");
+        this->goal_handle = goal_handle_;
+    }
+
+}
+
+
+void GoTo::goal_result_callback(const GoalHandleNav::WrappedResult &result)
+{
+    if(result.result)
+    {
+        done_flag=true;
+    }
+}
+void GoTo::goal_feedback_callback(
+    GoalHandleNav::SharedPtr,
+    const std::shared_ptr<const NavigateToPose::Feedback> feedback)
+{
+    (void)feedback;
+}
+
+
+void GoTo::team_color_callback(const std_msgs::msg::Int8 &msg)
+{
+    if( msg.data == -1)
+        team_color = RED;
+    else
+        team_color = BLUE;
+    
+    color_feedback_publisher->publish(msg);
+
+}
+
+void GoTo::cancel_goal()
+{
+    if (goal_handle )
+    {
+        RCLCPP_WARN(node_ptr_->get_logger(), "GoTo::No active goal to cancel");
+    }
+    else
+    {
+        RCLCPP_INFO(node_ptr_->get_logger(), "GoTo::Goal canceled");
+        try
+        {
+            auto cancel_future = action_client_ptr_->async_cancel_goal(goal_handle);
+        }
+        catch(rclcpp_action::exceptions::UnknownGoalHandleError)
+        {
+            RCLCPP_WARN(node_ptr_->get_logger(),"GoTo::cancel_goal::rclcpp_action::exceptions::UnknownGoalHandleError");
+        }
+    }
+
+}
+
+
+void GoTo::nav_to_pose_compute_goal()
+{
+    auto x_ = getInput<float>("x");
+    auto y_ = getInput<float>("y");
+    auto theta_ = getInput<float>("theta");
+
+    if( !x_ )
+    {
+        throw BT::RuntimeError("error reading port [x]:", x_.error());
+    }
+
+    if( !y_ )
+    {
+        throw BT::RuntimeError("error reading port [y]:", y_.error());
+    }
+
+    goal_msg.pose.header.frame_id = "map";
+    goal_msg.pose.pose.position.x = x_.value();
+    goal_msg.pose.pose.position.y = y_.value();
+    goal_msg.pose.pose.position.z = 0.0;
+
+    goal_msg.pose.pose.orientation.x = 0.0;
+    goal_msg.pose.pose.orientation.y = 0.0;
+    goal_msg.pose.pose.orientation.z = 0.0;
+    goal_msg.pose.pose.orientation.w = 1.0;
+}
+
